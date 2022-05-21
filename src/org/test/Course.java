d2896e7cccde59cc6d98de5e673987ae3ae3bc03
samjonam7 <samjonam7@gmail.com>
2022-05-21 14:30:10 +0530

    new code added

---
+++ src/org/test/Course.java
@@ -0,0 +1,12 @@
+package org.test;
+
+public class Course {
+	public void javaCourse() {
+		System.out.println("Java");
+		
+	}
+	public static void main(String[] args) {
+		Course c = new Course();
+		c.javaCourse();
+	}
+}
